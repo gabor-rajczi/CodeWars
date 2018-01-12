ee33196cbd9c57d47b3e2441d69a13d4dd6df09e
rajczig <rajczi.gabor@dexter.hu>
2018-01-12 15:25:57 +0100

    Can not send as a ValidParentheses solution

---
+++ codewars/ValidParentheses/Parentheses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.ValidParentheses
+{
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            if (input == "")
+                return true;
+            var listOfChars = input.ToCharArray();
+            int countOfOpenParentheses = 0;
+            int countOfCloseParentheses = 0;
+            for (var i = 0; i < listOfChars.Length; i++)
+            {
+                countOfOpenParentheses += listOfChars[i] == '(' ? 1 : 0;
+                countOfCloseParentheses += listOfChars[i] == ')' ? 1 : 0;
+                if (countOfCloseParentheses > countOfOpenParentheses)
+                    return false;
+            }
+            return countOfOpenParentheses == countOfCloseParentheses;
+        }
+    }
+}
---
+++ codewars/ValidParentheses/SolutionTest.cs
@@ -0,0 +1,44 @@
+﻿namespace CodeWars.ValidParentheses
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("()"));
+        }
+
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses(")(((("));
+        }
+
+        [Test]
+        public void SampleTest3()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses("("));
+        }
+
+        [Test]
+        public void SampleTest4()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses("(())((()())())"));
+        }
+        [Test]
+        public void SampleTest5()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses("())("));
+        }
+
+        [Test]
+        public void SampleTest6()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses(" (ff(gg)df)f((()())())"));
+        }
+    }
+}
