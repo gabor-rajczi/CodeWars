4678d12ad105676641500df09382d024ff8aa115
rajczig <rajczi.gabor@dexter.hu>
2018-02-28 12:07:31 +0100

    LeastCommonMultiple

---
+++ codewars/LeastCommonMultiple/LeastCommonMultiple.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.LeastCommonMultiple
+{
+    public class LeastCommonMultiple
+    {
+        public static int Calculate(int num1, int num2)
+        {
+            int lcm = 0;
+            var x = num1;
+            var y = num2;
+            while (num1 != num2)
+            {
+                if (num1 > num2)
+                {
+                    num1 = num1 - num2;
+                }
+                else
+                {
+                    num2 = num2 - num1;
+                }
+            }
+            lcm = (x * y) / num1;
+            return lcm;
+        }
+
+        public static int Calculate(int[] numbers)
+        {
+            if(numbers.Length<2) throw new ArgumentException("Too short input array!");
+            while (true)
+            {
+                if (numbers.Length <= 2) return Calculate(numbers[0], numbers[1]);
+                var lcm = Calculate(numbers[0], numbers[1]);
+                numbers = numbers.Where((value, index) => index != 0).ToArray();
+                numbers[0] = lcm;
+            }
+        }
+    }
+}
---
+++ codewars/LeastCommonMultiple/LeastCommonMultipleTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace CodeWars.LeastCommonMultiple
+{
+    [TestFixture]
+    public class LeastCommonMultipleTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(2, LeastCommonMultiple.Calculate(1, 2));
+            Assert.AreEqual(6, LeastCommonMultiple.Calculate(2, 3));
+            Assert.AreEqual(12, LeastCommonMultiple.Calculate(3, 4));
+            Assert.AreEqual(18, LeastCommonMultiple.Calculate(2, 9));
+        }
+
+        [Test]
+        public void BasicArrayTests()
+        {
+            Assert.AreEqual(2, LeastCommonMultiple.Calculate(new [] {1, 2}));
+            Assert.AreEqual(6, LeastCommonMultiple.Calculate(new [] {2, 3}));
+            Assert.AreEqual(12, LeastCommonMultiple.Calculate(new [] {3, 4}));
+            Assert.AreEqual(18, LeastCommonMultiple.Calculate(new [] {2, 9}));
+        }
+
+        [Test]
+        public void ArrayTests()
+        {
+            Assert.AreEqual(6, LeastCommonMultiple.Calculate(new[] { 1, 2, 3 }));
+            Assert.AreEqual(12, LeastCommonMultiple.Calculate(new[] { 2, 3, 4 }));
+            Assert.AreEqual(12, LeastCommonMultiple.Calculate(new[] { 3, 4, 4 }));
+            Assert.AreEqual(18, LeastCommonMultiple.Calculate(new[] { 2, 9, 3 }));
+        }
+
+        [Test]
+        public void LongArrayTests()
+        {
+            Assert.AreEqual(8*9, LeastCommonMultiple.Calculate(new[] {3, 9, 8, 2, 6, 12}));
+        }
+
+        [Test]
+        public void ExceptionArrayTests()
+        {
+            var array = new[] {3};
+            ActualValueDelegate<int> testDelegate = () => LeastCommonMultiple.Calculate(array);
+            Assert.That(testDelegate, Throws.TypeOf<ArgumentException>());
+            array = new int[0];
+            Assert.That(testDelegate, Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
