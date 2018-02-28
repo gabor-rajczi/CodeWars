22c620b54af4c36e24b7a9718280d60c14c8d024
rajczig <rajczi.gabor@dexter.hu>
2018-02-28 08:53:02 +0100

    BuildTower

---
+++ codewars/BuildTower/BuildTower.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeWars.BuildTower
+{
+    public class BuildTower
+    {
+        public static string[] Build(int numberOfFloors)
+        {
+            var result = new string[numberOfFloors];
+            for (var i = 0; i < numberOfFloors; i++)
+            {
+                var floor = new string('*', i * 2 + 1);
+                floor = floor.PadLeft(numberOfFloors + i);
+                floor = floor + new string(' ', numberOfFloors - (i + 1));
+                result[i] = floor;
+            }
+            return result;
+        }
+    }
+
+    public class Kata
+    {
+        public static string[] TowerBuilder(int nFloors)
+        {
+            var result = new string[nFloors];
+            for (var i = 0; i < nFloors; i++)
+            {
+                var floor = new string('*', i * 2 + 1);
+                floor = floor.PadLeft(nFloors + i);
+                floor = floor + new string(' ', nFloors - (i + 1));
+                result[i] = floor;
+            }
+            return result;
+        }
+    }
+}
---
+++ codewars/BuildTower/BuildTowerTest.cs
@@ -0,0 +1,45 @@
+﻿namespace CodeWars.BuildTower
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BuildTowerTests
+    {
+        [Test]
+        public void BuildTowerWith3Floors()
+        {
+            var excepted = new string[]
+            {"  *  ", 
+             " *** ", 
+             "*****"
+            };
+            Assert.That(BuildTower.Build(3), Is.EquivalentTo(excepted));
+        }
+
+        [Test]
+        public void BuildTowerWith6Floors()
+        {
+            var excepted = new string[]
+            {"     *     ", 
+             "    ***    ", 
+             "   *****   ",
+             "  *******  ",
+             " ********* ",
+             "***********"
+            };
+            Assert.That(BuildTower.Build(6), Is.EquivalentTo(excepted));
+        }
+    }
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", Kata.TowerBuilder(1)));
+            Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", Kata.TowerBuilder(2)));
+            Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", Kata.TowerBuilder(3)));
+        }
+    }
+}
