f277b67c27e2968877101c476b8a27b8a0ab4b68
rajczig <rajczi.gabor@dexter.hu>
2018-03-29 14:52:11 +0200

    Simple Pig Latin

---
+++ codewars/SimplePigLatin/SimplePigLatin.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars.SimplePigLatin
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class SimplePigLatin
+    {
+        public static string PigIt2(string str)
+        {
+            var wordList = str.Split(' ').ToList();
+            var newWordList = new List<string>();
+            foreach (var word in wordList)
+            {
+                var newWord = word;
+                var punctuationMark = "";
+                if (char.IsPunctuation(word, word.Length - 1))
+                {
+                    punctuationMark = word.ToCharArray().Last().ToString();
+                    newWord = word.Length - 2>=0 ? word.Substring(0, word.Length - 2) : punctuationMark;
+                }
+                newWord = newWord.Length>1 ? newWord.Substring(1, newWord.Length - 1) + newWord.Substring(0, 1) + "ay" + punctuationMark : newWord;
+                newWordList.Add(newWord);
+            }
+            return string.Join(" ", newWordList);
+        }
+
+        public static string PigIt(string str)
+        {
+            //solution 1:
+            //return string.Join(" ", str.Split().Select(x => x.Substring(1) + x[0] + "ay")); //fail with punktuation
+            //solution 2:
+            //return Regex.Replace(str, "(?<=^| )(\\w)(\\w+)", m => m.Groups[2].Value + m.Groups[1].Value + "ay");
+            return Regex.Replace(str, @"(\w)(\w+)", @"$2$1ay");
+        }
+    }
+}
---
+++ codewars/SimplePigLatin/SimplePigLatinTest.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWars.SimplePigLatin
+{
+    using System;
+    using NUnit.Framework;
+    using Kata = SimplePigLatin;
+
+    [TestFixture]
+    class SimplePigLatinTest
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", Kata.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", Kata.PigIt("This is my string"));
+            Assert.AreEqual("elloHay orldWay !", Kata.PigIt("Hello World !"));
+        }
+    }
+}
