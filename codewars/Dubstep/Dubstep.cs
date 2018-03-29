c41bd7d3b147abfeeaf686d369a6620c7c15989d
rajczig <rajczi.gabor@dexter.hu>
2018-03-29 10:14:23 +0200

    Dubstep
    Decoding Song

---
+++ codewars/Dubstep/Dubstep.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CodeWars.Dubstep
+{
+    public class Dubstep
+    {
+        public static string SongDecoder2(string input)
+        {
+            if (input.StartsWith("WUB"))
+            {
+                return SongDecoder(input.Substring(3));
+            }
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input.Substring(i).StartsWith("WUB"))
+                {
+                    return (input.Substring(0, i) + " " + SongDecoder(input.Substring(i))).Trim();
+                }
+            }
+            return input.Trim();
+        }
+
+        public static string SongDecoder3(string input)
+        {
+            var result = input.Split(new string[]{"WUB"}, StringSplitOptions.RemoveEmptyEntries);
+            return string.Join(" ", result);
+        }
+
+        public static string SongDecoder(string input)
+        {
+            return Regex.Replace(input, "(WUB)+", " ").Trim();
+        }
+    }
+}
---
+++ codewars/Dubstep/DubstepTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars.Dubstep
+{
+    [TestFixture]
+    public class DubstepTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var result = Dubstep.SongDecoder("WUBWUBABCWUB");
+          Assert.AreEqual("ABC", result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+          Assert.AreEqual("R L", Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
+        }
+    }
+}
