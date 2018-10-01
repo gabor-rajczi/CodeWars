29bf9af2df118935899e9ceabd175f616e4a482c
rajczig <rajczi.gabor@dexter.hu>
2018-10-01 11:02:17 +0200

    Reflection #5 - Call me back

---
+++ codewars/Reflection/CallMeBack/Reflection.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CodeWars.Reflection.CallMeBack
+{
+    using System;
+    using System.Reflection;
+    using System.Diagnostics;
+
+    public static class Reflection
+    {
+        public static void Activator()
+        {
+            var stackTrace = new StackTrace();
+            var type = stackTrace.GetFrame(1).GetMethod().ReflectedType;
+            if(type == null)
+                return;
+            var staticMethods = type.GetMethods(BindingFlags.Static|BindingFlags.Public);
+            if(staticMethods.Length != 1)
+                return;
+            staticMethods[0].Invoke(null, null);
+        }
+    }
+}
---
+++ codewars/Reflection/CallMeBack/ReflectionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars.Reflection.CallMeBack
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ReflectionTests
+    {
+        private static bool setting;
+
+        public static void Activate()
+        {
+            setting = true;
+        }
+
+        [Test]
+        public void Test()
+        {
+            Reflection.Activator();
+            Assert.IsTrue(setting);
+        }
+    }
+}
