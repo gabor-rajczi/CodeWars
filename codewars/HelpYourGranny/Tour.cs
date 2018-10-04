841fab4b105823550bd512154a6f426895f85b3d
rajczig <rajczi.gabor@dexter.hu>
2018-10-04 08:51:55 +0200

    Help your granny!

---
+++ codewars/HelpYourGranny/Tour.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeWars.HelpYourGranny
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    public class Tour
+    {
+        public static int tour(string[] arrFriends, string[][] ftwns, Hashtable h)
+        {
+            var travelledDistance = 0.0;
+            var travelFrom = "Home";
+            var travelTo = "";
+            for (var i = 0; i < arrFriends.Length; i++)
+            {
+                var friendLiveInTown = ftwns.FirstOrDefault(t => t[0] == arrFriends[i]);
+                if (friendLiveInTown != null)
+                {
+                    travelTo = friendLiveInTown[1];
+                    var distanceFromHomeToDestination = (double) h[travelTo];
+                    if (i == 0)
+                        travelledDistance += distanceFromHomeToDestination;
+                    else
+                    {
+                        var distanceFromHomeToDeparture = (double) h[travelFrom];
+                        travelledDistance += Math.Sqrt(Math.Pow(distanceFromHomeToDestination,2) - Math.Pow(distanceFromHomeToDeparture,2));
+                    }
+                    travelFrom = travelTo;
+                }
+            }
+            travelledDistance += (double) h[travelTo];
+            var result = (int)Math.Floor(travelledDistance);
+            return result;
+        }
+    }
+}
---
+++ codewars/HelpYourGranny/TourTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace CodeWars.HelpYourGranny
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TourTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            string[] friends1 = new string[] {"A1", "A2", "A3", "A4", "A5"};
+            string[][] fTowns1 =
+            {
+                new string[] {"A1", "X1"}, new string[] {"A2", "X2"}, new string[] {"A3", "X3"},
+                new string[] {"A4", "X4"}
+            };
+            Hashtable distTable1 = new Hashtable {{"X1", 100.0}, {"X2", 200.0}, {"X3", 250.0}, {"X4", 300.0}};
+            Assert.AreEqual(889, Tour.tour(friends1, fTowns1, distTable1));
+        }
+    }
+
+}
